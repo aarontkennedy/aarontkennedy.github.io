4e627463e2d9fae559b131d0df73bb22884ff376
Aaron Kennedy <aarontkennedy@MacBook-Air.local>
2018-06-12 08:54:59 -0500

    added a project to the portfolio, included a better background image, added tech tags to narrow down projects in the portfolio

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 $(document).foundation();
+
+$(document).ready(function () {
+    let allPortfolioItems = $(".portfolioItems li");
+
+    $("#technologies").on("click", "li", function (e) {
+        e.preventDefault();
+        let techClass = $(this).text().toLowerCase().replace(/\W/g, '');
+
+        switch (techClass) {
+            case "showall":
+                allPortfolioItems.show();
+                break;
+            default:
+                allPortfolioItems.hide();
+                $("."+techClass).show();
+        }
+
+    });
+
+
+});
